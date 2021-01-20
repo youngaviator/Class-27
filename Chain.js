b0bc15a81f7557746ea939d838519140f0a631aa
youngaviator <75797837+youngaviator@users.noreply.github.com>
2021-01-20 19:00:50 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,18 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.04,
+            length:10
+        }
+       this.chain= Constraint.create(options);
+        World.add(world,this.chain)
+
+    }
+
+display(){
+    line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y) 
+}
+    
+}
